3af932d972368d0209fcf9b4fed91c77b3420311
allard.kazuki <kazutor73@gmail.com>
2022-09-06 11:56:59 +0200

    add constructor

---
+++ Souris.java
@@ -0,0 +1,22 @@
+
+public class Souris {
+
+    private int _poids;
+    private int _age;
+    private String _couleur;
+    private int _esperanceVie;
+    private boolean _clonee;
+
+    public Souris(int unPoids, int unAge, String uneCouleur, int uneEsperanceVie, boolean unClonee) {
+        this._poids = unPoids;
+        this._age = unAge;
+        this._couleur = uneCouleur;
+        this._esperanceVie = uneEsperanceVie;
+        this._clonee = unClonee;
+    }
+
+    public Souris(int unPoids, String uneCouleur, boolean unClonee) {
+
+    }
+
+}
